872046ecad48568e94ad97d4422c1a36693935ec
madisonslayden <93334609+madisonslayden@users.noreply.github.com>
2025-10-02 18:48:51 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("register.html")
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form["name"]
+    blood = request.form["blood"]
+    contact = request.form["contact"]
+    info = request.form["info"]
+    
+    print(f"Name: {name}, Blood: {blood}, Contact: {contact}, Info: {info}")
+
+    return f"Thanks {name}, your Digilicense has been registered!"
+  
+  if __name__ == "__main__":
+    app.run(debug=True)
