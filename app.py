c5f7b4f55f13f286a3ea51bd47e029d32df7ea85
madisonslayden <93334609+madisonslayden@users.noreply.github.com>
2025-10-02 19:02:09 -0500

    Update app.py
    Indention mistake

--- app.py
+++ app.py
@@ -5,16 +5,19 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("register.html")
+
 @app.route("/register", methods=["POST"])
 def register():
     name = request.form["name"]
     blood = request.form["blood"]
     contact = request.form["contact"]
     info = request.form["info"]
+
     
     print(f"Name: {name}, Blood: {blood}, Contact: {contact}, Info: {info}")
 
     return f"Thanks {name}, your Digilicense has been registered!"
-  
-  if __name__ == "__main__":
+
+
+if __name__ == "__main__":
     app.run(debug=True)
