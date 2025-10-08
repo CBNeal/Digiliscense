a76752ef8c54883368f9901ba2e7fd8cbd8c16ba
Callum <callumbneal@gmail.com>
2025-10-07 21:56:45 -0500

    Merged Sites Recieve info and display info

--- WEBDEV/app.py
+++ WEBDEV/app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return render_template("register.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    name = request.form["name"]
+    blood = request.form["blood"]
+    contact = request.form["contact"]
+    info = request.form["info"]
+    
+    print(f"Name: {name}, Blood: {blood}, Contact: {contact}, Info: {info}")
+    context = {'name':name,
+               'blood':blood,
+               'contact':contact,
+               'info':info}
+    return f"Thanks {name}, your Digilicense has been registered! To display your information, add [/USER] to the end of this URL", jsonify(context)
+
+
+
+'''
 @app.route('/')
 def home():
     return render_template('index.html')
-
+'''
 '''
 @app.route('/submit', methods = ['POST'])
 def submit():
@@ -13,9 +34,9 @@ def submit():
     return f"hello {name}! Thanks for submitting the form"
 '''
 ###This app route will provide the user information for when a nfc tag is scanned
-@app.route('/test')
+@app.route('/USER')
 def test():
-    return "This works" 
+    return render_template("index.html") 
 
 
 if __name__ == '__main__':
