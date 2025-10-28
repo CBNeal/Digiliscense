194fbd5b1068e20309782403a74404dfcd1f0fbb
Maddy <madisonslayden6@gmail.com>
2025-10-28 15:28:10 -0500

    made 4 digits to 6 and fixed security issues

--- WEBDEV/app.py
+++ WEBDEV/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import random 
 
 app = Flask(__name__)
 
@@ -16,7 +17,7 @@ db = SQLAlchemy(app)
 ################
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    pin = db.Column(db.String(5), nullable = False)
+    pin = db.Column(db.String(6), nullable = False, unique = True)
     name = db.Column(db.String(50), nullable = False)
     bloodtype = db.Column(db.String(10), nullable = False)
     contact = db.Column(db.String(50), nullable = False)
@@ -24,11 +25,13 @@ class User(db.Model):
 
 with app.app_context():
     db.create_all()
+
 ## This is for the "homebase", opening page - C
 @app.route("/")
 def home():
     return render_template("register.html")
 
+#Access user info by PIN
 @app.route("/access", methods=["POST"])
 def access():
     pin = request.form['pin'].strip()
@@ -39,13 +42,13 @@ def access():
     else:
         return "❌ Invalid PIN. User not found.", 404
 
+#Register New User
 @app.route("/register", methods=["POST"])
 def register():
     # Debug print
     print("FORM DATA RECEIVED:", request.form)
 
     # Grab and strip form fields
-    pin = request.form.get("pin", "").strip()
     name = request.form.get("name", "").strip()
     blood = request.form.get("blood", "").strip()
     contact = request.form.get("contact", "").strip()
@@ -54,25 +57,20 @@ def register():
     # Validation
     if not name or not blood or not contact:
         return "Please fill out all required fields., User will not work properly if all fields are not filled", 400
+    
+    # Generate a unique 6-digit PIN
+    pin = str(random.randint(100000, 999999))
+    
 
     # Save to database
-    new_user = User(name = name, pin = pin,  bloodtype = blood, contact = contact, info = info)
+    new_user = User(name = name, pin = pin,  bloodtype = blood, contact = contact, info = info or None)
     db.session.add(new_user)
     db.session.commit()
 
     print(f"✅ Saved user: {name}, {pin}, {blood}, {contact}, {info}, User saved into SQLITE file")
 
-    return redirect(url_for("user_list"))
+    return render_template("index.html", pin=pin)
 
-
-@app.route('/user/<int:user_id>')
-def get_user(user_id):
-    user = User.query.filter_by(pin=pin).first()
-
-    if user:
-        return render_template('user.html', user=user)
-    else:
-        return f"User with ID {user_id} not found. Check again and see if the Code on the DIGILISCENSE is correct", 404
 
 ### For showcasing the user  page 
 @app.route("/USER")
